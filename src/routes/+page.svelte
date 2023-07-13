61ada564ed268731e3b400e38bcf3c185885beb2
Jeff Zhao <jeff.no.zhao@gmail.com>
2023-07-12 23:49:45 -0400

    add meta tags

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,20 @@
 <script lang="ts">
+	import { MetaTags } from 'svelte-meta-tags';
+
 	import { PRIMARY_BUTTON_STYLE } from '$lib/styles/button';
 </script>
+
+<MetaTags
+	title="Ammonite Docs"
+	description="Technical documentation on the Ammonite platform"
+	canonical="https://docs.ammonite.dev"
+	openGraph={{
+		url: 'https://docs.ammonite.dev',
+		title: 'Ammonite Docs',
+		description: 'Technical documentation on the Ammonite platform',
+		site_name: 'Ammonite Docs'
+	}}
+/>
 
 <div class="flex flex-col items-center justify-center">
 	<div class="h-12" />
