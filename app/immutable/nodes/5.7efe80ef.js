29990b5de738696f4a45884278e856ffe8e64a30
kamiyaa <kamiyaa@users.noreply.github.com>
2023-07-19 22:59:56 +0000

    deploy: 0cbd735c05980d0b0b5bdd79f4a814d03a350789

---
+++ app/immutable/nodes/5.7efe80ef.js
@@ -0,0 +1,16 @@
+import{S as V,i as j,s as z,k as l,q as r,a as N,l as i,m as c,r as d,h as n,c as q,n as v,b as B,C as e,D as I}from"../chunks/index.44e2991a.js";import{c as F}from"../chunks/syntax.d8b6a914.js";function G(W){let t,u,g,E,a,b,h,T,y,p,H,P,m,k,x,A,_,f,O=F("html",`<html>
+	<head>
+		<!-- content -->
+	</head>
+	<body>
+		<!-- content -->
+	</body>
+</html>`)+"";return{c(){t=l("div"),u=l("h1"),g=r("Website 101"),E=N(),a=l("p"),b=r(`This section is geared towards an audience who has little to no knowledge of how websites work.
+		In particular, HTML elements and their purposes. In general, an HTML document has the following
+		structure. An `),h=l("span"),T=r("<html>"),y=r(` element with a
+		`),p=l("span"),H=r("<head>"),P=r(`
+		element and `),m=l("span"),k=r("<body>"),x=r(" element inside it."),A=N(),_=l("pre"),f=l("code"),this.h()},l(w){t=i(w,"DIV",{class:!0});var s=c(t);u=i(s,"H1",{});var L=c(u);g=d(L,"Website 101"),L.forEach(n),E=q(s),a=i(s,"P",{});var o=c(a);b=d(o,`This section is geared towards an audience who has little to no knowledge of how websites work.
+		In particular, HTML elements and their purposes. In general, an HTML document has the following
+		structure. An `),h=i(o,"SPAN",{class:!0});var M=c(h);T=d(M,"<html>"),M.forEach(n),y=d(o,` element with a
+		`),p=i(o,"SPAN",{class:!0});var S=c(p);H=d(S,"<head>"),S.forEach(n),P=d(o,`
+		element and `),m=i(o,"SPAN",{class:!0});var C=c(m);k=d(C,"<body>"),C.forEach(n),x=d(o," element inside it."),o.forEach(n),A=q(s),_=i(s,"PRE",{});var D=c(_);f=i(D,"CODE",{});var R=c(f);R.forEach(n),D.forEach(n),s.forEach(n),this.h()},h(){v(h,"class","inline_code"),v(p,"class","inline_code"),v(m,"class","inline_code"),v(t,"class","tech_doc")},m(w,s){B(w,t,s),e(t,u),e(u,g),e(t,E),e(t,a),e(a,b),e(a,h),e(h,T),e(a,y),e(a,p),e(p,H),e(a,P),e(a,m),e(m,k),e(a,x),e(t,A),e(t,_),e(_,f),f.innerHTML=O},p:I,i:I,o:I,d(w){w&&n(t)}}}class Q extends V{constructor(t){super(),j(this,t,null,G,z,{})}}export{Q as component};
