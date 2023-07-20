4da16df18e4a63911c9e9149c54cb81162552eed
kamiyaa <kamiyaa@users.noreply.github.com>
2023-07-20 04:30:41 +0000

    deploy: 05abe9c07fa97e1dd98c0ac9292a0824a2063516

---
+++ app/immutable/nodes/25.066ba143.js
@@ -0,0 +1,34 @@
+import{S as we,i as ye,s as ve,k as n,q as i,a as m,l as s,m as o,r as l,h as a,c as d,n as b,b as be,C as e,D as ne}from"../chunks/index.98343197.js";import{c as Ee}from"../chunks/syntax.d8b6a914.js";import{C as ue}from"../chunks/company.5e87be2a.js";import{A as Te}from"../chunks/routes.b1c9d26b.js";function Ae(fe){let t,u,C,_,D,S,W,p,U,w,q,G,y,O,R,h,E,V,j,T,Y,z,A,B,F,M,J,K,L,Q,f,X,Z,v,$,ee,k,te,ae,P,x,ge=Ee("html",`<html>
+	<head>
+		<title>My Webpage Title</title>
+
+		<meta name="description" content="My first webpage using Ammonite">
+
+		<meta name="twitter:image:src" content="https://link_to_image">
+		<meta name="twitter:site" content="@abc">
+		<meta name="twitter:card" content="summary_large_image">
+		<meta name="twitter:title" content="My Webpage Title">
+		<meta name="twitter:description" content="My first webpage using Ammonite">
+
+      	<meta property="og:image" content="https://link_to_image">
+		<meta property="og:image:alt" content="My first webpage using Ammonite">
+		<meta property="og:site_name" content="Ammonite">
+		<meta property="og:type" content="website">
+		<meta property="og:title" content="My Webpage Title">
+		<meta property="og:url" content="https://ammonite.dev/proj/test/p/test_page">
+		<meta property="og:description" content="My first webpage using Ammonite">
+	</head>
+	<body>
+		<!-- content -->
+	</body>
+</html>`)+"";return{c(){t=n("div"),u=n("h1"),C=i("The "),_=n("span"),D=i("head"),S=i(" Element"),W=m(),p=n("p"),U=i("Nearly all websites have a "),w=n("span"),q=i("<head>"),G=i(`. This element is
+		responsible for everything that does not appear on the page. Users can place elements within the
+		`),y=n("span"),O=i("<head>"),R=i(`
+		element to configure many aspects of the website. This includes, but not limited to:
+		`),h=n("uL"),E=n("li"),V=i("The title of the page (what you see on your tabs)"),j=m(),T=n("li"),Y=i("The thumbnail preview of the page (when you share links with friends)"),z=m(),A=n("li"),B=i("The preview description of the page (when you share links with friends)"),F=m(),M=n("li"),J=i("Whether a bot should scrape this website (like Google's search engine)"),K=m(),L=n("div"),Q=m(),f=n("p"),X=i(ue),Z=i(` allow users to configure these using a custom built-in template:
+		`),v=n("a"),$=i("ammonite_head"),ee=m(),k=n("h4"),te=i("Example HTML"),ae=m(),P=n("pre"),x=n("code"),this.h()},l(N){t=s(N,"DIV",{class:!0});var r=o(t);u=s(r,"H1",{});var H=o(u);C=l(H,"The "),_=s(H,"SPAN",{class:!0});var se=o(_);D=l(se,"head"),se.forEach(a),S=l(H," Element"),H.forEach(a),W=d(r),p=s(r,"P",{});var g=o(p);U=l(g,"Nearly all websites have a "),w=s(g,"SPAN",{class:!0});var oe=o(w);q=l(oe,"<head>"),oe.forEach(a),G=l(g,`. This element is
+		responsible for everything that does not appear on the page. Users can place elements within the
+		`),y=s(g,"SPAN",{class:!0});var ie=o(y);O=l(ie,"<head>"),ie.forEach(a),R=l(g,`
+		element to configure many aspects of the website. This includes, but not limited to:
+		`),h=s(g,"UL",{});var c=o(h);E=s(c,"LI",{});var le=o(E);V=l(le,"The title of the page (what you see on your tabs)"),le.forEach(a),j=d(c),T=s(c,"LI",{});var re=o(T);Y=l(re,"The thumbnail preview of the page (when you share links with friends)"),re.forEach(a),z=d(c),A=s(c,"LI",{});var he=o(A);B=l(he,"The preview description of the page (when you share links with friends)"),he.forEach(a),F=d(c),M=s(c,"LI",{});var pe=o(M);J=l(pe,"Whether a bot should scrape this website (like Google's search engine)"),pe.forEach(a),c.forEach(a),g.forEach(a),K=d(r),L=s(r,"DIV",{class:!0}),o(L).forEach(a),Q=d(r),f=s(r,"P",{});var I=o(f);X=l(I,ue),Z=l(I,` allow users to configure these using a custom built-in template:
+		`),v=s(I,"A",{href:!0});var ce=o(v);$=l(ce,"ammonite_head"),ce.forEach(a),I.forEach(a),ee=d(r),k=s(r,"H4",{});var me=o(k);te=l(me,"Example HTML"),me.forEach(a),ae=d(r),P=s(r,"PRE",{});var de=o(P);x=s(de,"CODE",{});var _e=o(x);_e.forEach(a),de.forEach(a),r.forEach(a),this.h()},h(){b(_,"class","inline_code"),b(w,"class","inline_code"),b(y,"class","inline_code"),b(L,"class","h-4"),b(v,"href",Te.DocsTemplatesAmmoniteHead),b(t,"class","tech_doc")},m(N,r){be(N,t,r),e(t,u),e(u,C),e(u,_),e(_,D),e(u,S),e(t,W),e(t,p),e(p,U),e(p,w),e(w,q),e(p,G),e(p,y),e(y,O),e(p,R),e(p,h),e(h,E),e(E,V),e(h,j),e(h,T),e(T,Y),e(h,z),e(h,A),e(A,B),e(h,F),e(h,M),e(M,J),e(t,K),e(t,L),e(t,Q),e(t,f),e(f,X),e(f,Z),e(f,v),e(v,$),e(t,ee),e(t,k),e(k,te),e(t,ae),e(t,P),e(P,x),x.innerHTML=ge},p:ne,i:ne,o:ne,d(N){N&&a(t)}}}class xe extends we{constructor(t){super(),ye(this,t,null,Ae,ve,{})}}export{xe as component};
